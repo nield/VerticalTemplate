7ed37d69356a6c281f6d88f51927a6ee7b894ff1
Niel de Wet <nieldw@gobalkinetic.com>
2025-05-19 16:19:02 +0200

    feat: add v2 endpoint

--- src/VerticalTemplate.Api/Configurations/FastEndpoints.cs
+++ src/VerticalTemplate.Api/Configurations/FastEndpoints.cs
@@ -12,9 +12,23 @@ internal static class FastEndpoints
         .SwaggerDocument(opt =>
         {
             opt.MaxEndpointVersion = 1;
+            opt.MinEndpointVersion = 1;
             opt.DocumentSettings = s =>
             {
                 s.Version = "v1";
+                s.DocumentName = "v1";
+                s.Title = "API";
+                s.Description = "API Template";
+            };
+        })
+        .SwaggerDocument(opt =>
+        {
+            opt.MaxEndpointVersion = 2;
+            opt.MinEndpointVersion = 2;
+            opt.DocumentSettings = s =>
+            {
+                s.Version = "v2";
+                s.DocumentName = "v2";
                 s.Title = "API";
                 s.Description = "API Template";
             };
---
+++ tests/VerticalTemplate.Api.Integration.Tests/Features/ToDos/V2/ToDoTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+
+namespace VerticalTemplate.Api.Integration.Tests.Features.ToDos.V2;
+
+[Collection("WebApplicationCollection")]
+public class ToDoTests
+{
+    private readonly WebApplicationFixture _webApplicationFixture;
+
+    public ToDoTests(WebApplicationFixture webApplicationFixture)
+    {
+        _webApplicationFixture = webApplicationFixture;
+    }
+
+    [Fact]
+    public async Task GetAll_Should_ReturnData()
+    {
+        var sut = await _webApplicationFixture.HttpClient.GetFromJsonAsync<IEnumerable<Api.Features.ToDos.V2.GetAll.Response>>(
+            "/api/v2/todos");
+
+        Assert.NotNull(sut);
+        Assert.NotEmpty(sut);
+    }
+}
